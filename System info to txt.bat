c168ae9c205bc2c581eaa2b20741536bfd1308a4
LukasPavlas <lukas.pavlas@gmail.com>
2024-02-27 17:46:57 +0100

    Delete System info to txt.bat

--- System info to txt.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set hostname 
-hostname
-echo %hostname%
-pause
-exit
-systeminfo >C:%UserProfile%\Systeminfo.txt
-ipconfig /all >>%UserProfile%\Systeminfo.txt
-wmic bios get serialnumber | more >> %UserProfile%\Desktop\Systeminfo.txt
